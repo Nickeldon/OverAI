f08f4280812061bce48ce6d9c910e74064d992b6
Nickeldon <50726216+Nickeldon@users.noreply.github.com>
2024-06-10 02:46:55 -0400

    Initial Commit (Was Private sourced before)

---
+++ electron.vite.config.js
@@ -0,0 +1,12 @@
+// electron.vite.config.js
+export default {
+    main: {
+      // vite config options
+    },
+    preload: {
+      // vite config options
+    },
+    renderer: {
+      // vite config options
+    }
+  }
